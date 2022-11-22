e191f28abeca5c5f985199ca68b61532f52b1dc4
RANIT SARKAR <85814394+RANIT001@users.noreply.github.com>
2022-11-22 20:07:45 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 # list comprehensions
+<<<<<<< HEAD
 num_list= [1,2,3,4,5,6,7,8,9]
 #new_list= [new_item for item in list]
 new_list=[n+1 for n in num_list]
 print(new_list)
+=======
+#num_list= [1,2,3,4,5,6,7,8,9]
+#new_list= [new_item for item in list]
+#new_list=[n+1 for n in num_list]
+#print(new_list)
+#
+# string="Ranit"
+# letter_list = [letter for letter in string]
+# print(letter_list)
+#
+# list=[ n*2 for n in  range(1,5)]
+# print(list)
+
+# new_list = [new_item for item in list if test]
+
+>>>>>>> bde8368 (Initial commit)
